a5157d9a5ce29e125038e4aa74f9815592f487fa
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-05 23:22:01 +0000

    feat: Add dummy user generation to `seeds.rb` file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,26 @@
-# Get all users in the database
-users = User.take(20)
-
-# Generate 5 posts, alternating between users
-10.times do |i|
-  Post.create(
-    title: Faker::Lorem.sentence(word_count: 3),
-    content: Faker::Lorem.paragraph(sentence_count: [ 100, 10, 500, 1000 ].sample),
-    user_id: users[i % users.length].id,  # Alternate between users
-    slug: Faker::Internet.slug,
-    status: [ "public", "private" ].sample,
+# Generate 100 users
+100.times do
+  User.create(
+    username: Faker::Internet.username,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "password1234",
   )
+end
+
+# Get all 100 users in the database
+users = User.all
+
+# Generate 5 posts for each user, alternating between users
+100.times do |i|
+  [ 10, 3, 1, 6 ].sample.times do
+    Post.create(
+      title: Faker::Lorem.sentence(word_count: [ 3, 5, 6 ].sample),
+      content: Faker::Lorem.paragraph(sentence_count: [ 100, 10, 500, 1000 ].sample),
+      user_id: users[i % users.length].id,  # Alternate between users
+      slug: Faker::Internet.slug,
+      status: [ "public", "private" ].sample,
+    )
+  end
 end
