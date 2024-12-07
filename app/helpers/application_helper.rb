b2ed8ad9b8edc1e5e716e2cbc14d04da4ce48c5c
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-07 10:41:39 +0000

    feat: Add a helper for status background selector

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def status_class(status)
+    case status
+    when "public"
+      "bg-success text-white"
+    when "private"
+      "bg-primary text-white"
+    when "archived"
+      "bg-secondary text-white"
+    when "draft"
+      "bg-warning text-dark"
+    else
+      "bg-dark text-white"
+    end
+  end
 end
