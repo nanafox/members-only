caaa2b2c2a5387e62803492c6a0512f6b759f0e0
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-08 16:22:39 +0000

    feat: Add methods to get the fullname and initials of a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :lockable
 
-  has_many :posts, dependent: :destroy
-  has_many :comments
+  has_many :posts, dependent: :delete_all
+  has_many :comments, dependent: :delete_all
+
+  def fullname
+    "#{first_name} #{last_name}"
+  end
+
+  def initials
+    "#{first_name.first}#{last_name.first}"
+  end
 end
