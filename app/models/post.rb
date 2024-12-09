60eaa22f914af5ce5f2177ea93d2f114122604b0
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-09 09:04:09 +0000

    chore: Add validation for the presence of the status field in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,5 @@ class Post < ApplicationRecord
   has_rich_text :content
   has_many :comments, dependent: :delete_all
 
-  validates :title, :content, presence: :true
+  validates :title, :content, :status, presence: :true
 end
