0760c0debe76386690ebfa02e0a43994a2225e94
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-10 10:43:24 +0000

    feat: Make post generation for users more random

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,16 +15,13 @@ User.create(
   first_name: "John", last_name: "Doe", password: "password",
 )
 
-# Get all 100 users in the database
-users = User.all
-
 # Generate posts for each user, alternating between users
 100.times do |i|
   [ 5, 3, 1, 6 ].sample.times do
     Post.create(
       title: Faker::Lorem.sentence(word_count: [ 3, 5, 6 ].sample),
       content: Faker::Lorem.paragraph(sentence_count: [ 100, 10, 500, 1000 ].sample),
-      user_id: users[i % users.length].id,  # Alternate between users
+      user_id: User.ids.sample,  # Alternate between users
       slug: Faker::Internet.slug,
       status: Visible::VALID_STATUSES.sample,
     )
