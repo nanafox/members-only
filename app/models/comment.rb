315f026bc44193127915209beff2a45865604756
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-11 19:44:05 +0000

    feat: Prepare Comment model to handle replies (self-join) implementation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,13 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates_associated :post, :user
+  validates :content, presence: true
+
+  has_many :replies, class_name: "Comment", foreign_key: "reply_id"
+
+  belongs_to :reply, class_name: "Comment", optional: true
+
+  def reply?
+    reply_id.present?
+  end
 end
