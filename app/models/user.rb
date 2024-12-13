771d075bff7c8aefafc584d7e1d6f42402824f8a
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-13 09:13:29 +0000

    style: Style up the alignment of the User model and add doc to #follows?

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :lockable
+    :recoverable, :rememberable, :validatable, :trackable, :lockable
 
   has_many :posts, dependent: :delete_all
   has_many :comments, dependent: :delete_all
 
-  validates :username, uniqueness: { message: "has already been taken, try another one" }
+  validates :username,
+    uniqueness: { message: "has already been taken, try another one" }
 
   # Users that this user is following
-  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :active_relationships,
+    class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :active_relationships, source: :followed
 
   # Users following this user
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :passive_relationships,
+    class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
 
   # Builds the full name for the current user using their first and last names.
@@ -28,7 +31,8 @@ class User < ApplicationRecord
     "#{first_name.first}#{last_name.first}"
   end
 
-  def follows?(other)
-    followed_users.include?(other)
+  # Checks whether a user is following another user
+  def follows?(user)
+    followed_users.load.include?(user)
   end
 end
